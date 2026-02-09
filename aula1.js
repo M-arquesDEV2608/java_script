5e842fb97f7448222127b67178d2f66bea006ede
Arthur Marques <arthurmarquesduartesilva@gmail.com>
2026-02-09 16:27:30 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,77 @@
+// Antigo, pode ser redeclarado e mudar de valor.
+//var idade = 10
+//var cor = "azul"
+//console.log(cor)
+
+//mais moderno, pode mudar de valor, mas não pode ser redeclarado.
+//let nome = "Rakya"
+//let fruta = "banana"
+//fruta = "maça" // pode mudar o valor, mas não pode ser redeclarado
+//console.log(fruta)
+
+// Fixo, não pode mudar de valor
+//const cpf = 0830495384095
+//const pi = 3.14
+//console.log(pi) 
+
+// Desafios
+
+// exercício 1
+var meunome = "Arthur"
+var minhaidade = 17
+
+console.log(meunome)
+console.log(minhaidade)
+
+// exercício 2
+let comidafavorita = "Danielle"
+comidafavorita = "Churrasco"
+console.log(comidafavorita)
+
+const paisnascimento = "Brasil"
+//paisnascimento = "Argentina"
+console.log(paisnascimento)
+
+// exercício 3
+var nome = "Arthur"
+var idade = 17 
+var cidade = "Guarulhos"
+console.log(`Nome: ${nome}, Idade: ${idade}, Cidade: ${cidade}`)
+
+// exercício 4 
+let curso = "DS"
+curso = "Mecatronica"
+console.log(curso)
+
+// exercício 5
+const anoatual = 2026
+//anoatual = 2019
+console.log(anoatual)
+
+// exercício 6
+var Nome = "Arthur"
+var sobrenome = "Marques"
+console.log(`Olá, me chamo ${Nome} ${sobrenome}`)
+
+// exercício 7
+var usuario = "Marcão"
+var email = "arthur.mds@gmail.com"
+var idade = 18
+console.log(`Usuario: ${nome} 
+E-mail: ${email} 
+Idade: ${idade}`)
+// exercício 8
+var num1 = 18
+var num2 = 23
+console.log(`${num1} ${num2}`)
+
+// exercício 9
+var nota1 = 8
+var nota2 = 6
+var nota3 = 7.5
+console.log(`Suas notas: ${nota1}, ${nota2}, ${nota3}`)
+
+// exercício 10
+var produto = "Steam Machine"
+var preco = 8.593
+console.log(`${produto} Preço R$ ${preco}`) 
